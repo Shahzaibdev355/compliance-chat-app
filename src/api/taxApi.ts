49d6254b502121d60a85de93db1a8b26263d84d5
Shahzaib <shahzaibsheikh355@gmail.com>
2026-06-30 02:55:42 +0500

    new change in taxapi file

--- src/api/taxApi.ts
+++ src/api/taxApi.ts
@@ -4,7 +4,7 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const taxApi = axios.create({
     baseURL: BASE_URL,
-    withCredentials: true,
+    // withCredentials: true,
     headers: {
         "Content-Type": "application/json",
     },
@@ -24,7 +24,7 @@ export const libraryApi = axios.create({
 
 export const LexaApi = axios.create({
     baseURL: BASE_URL,
-    withCredentials: true,
+    // withCredentials: true,
     headers: {
         "Content-Type": "application/json",
     },
